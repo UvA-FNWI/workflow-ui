57768edac17ae78909702f7eb9894ee9c7f68648
Annes Negmel-Din <annesnour03@gmail.com>
2025-11-03 12:34:56 +0100

    DN-3408 chore(style): format files

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { VITE_WEBAPI_URL, VITE_ENV } from './helpers/Environment'
+import {useState} from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import {VITE_WEBAPI_URL, VITE_ENV} from "./helpers/Environment";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <p>API URL: {VITE_WEBAPI_URL}</p>
-      <p>Environment: {VITE_ENV}</p>
-    </>
-  )
+    return (
+        <>
+            <div>
+                <a href="https://vite.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+            </div>
+            <h1>Vite + React</h1>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+            <p>API URL: {VITE_WEBAPI_URL}</p>
+            <p>Environment: {VITE_ENV}</p>
+        </>
+    );
 }
 
-export default App
+export default App;
--- src/helpers/Environment.tsx
+++ src/helpers/Environment.tsx
@@ -1,9 +1,9 @@
 interface CustomEnvWindow extends Window {
-  _env?: {
-    VITE_WEBAPI_URL?: string;
-    VITE_ENV?: string;
-  };
+    _env?: {
+        VITE_WEBAPI_URL?: string;
+        VITE_ENV?: string;
+    };
 }
 const envWindow = window as CustomEnvWindow;
 
-export const { VITE_WEBAPI_URL, VITE_ENV } = envWindow._env || {};
+export const {VITE_WEBAPI_URL, VITE_ENV} = envWindow._env || {};
