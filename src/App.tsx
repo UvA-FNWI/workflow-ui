dfe699e62c99274b5c28df65858ecd9943719f97
annes <76728727+annesnour03@users.noreply.github.com>
2025-11-10 16:27:00 +0100

    DN-3408 feat(config): add ESLint and Prettier configuration, build wo… (#4)
    * DN-3408 feat(config): add ESLint and Prettier configuration, build workflow
    
    * DN-3408 test with pnpm installation
    
    * DN-3408 debug: other steps
    
    * DN-3408 chore: better caching
    
    * DN-3408 chore: update translations check command and add workflow step for translations
    
    * DN-3408 chore(workflow): improve caching strategy by removing restore-keys for pnpm store
    
    * DN-3408 chore(style): format files

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import { Outlet } from "react-router";
+import {Outlet} from "react-router";
 import "./App.css";
-import { VITE_WEBAPI_URL, VITE_ENV } from "./helpers/Environment";
+import {VITE_WEBAPI_URL, VITE_ENV} from "./helpers/Environment";
 function App() {
-  return (
-    <div className="app">
-      <header className="header">
-        <h1>Workflow UI</h1>
-        <small>
-          {VITE_ENV} | {VITE_WEBAPI_URL}
-        </small>
-      </header>
-      <main className="main">
-        <Outlet />
-      </main>
-    </div>
-  );
+    return (
+        <div className="app">
+            <header className="header">
+                <h1>Workflow UI</h1>
+                <small>
+                    {VITE_ENV} | {VITE_WEBAPI_URL}
+                </small>
+            </header>
+            <main className="main">
+                <Outlet />
+            </main>
+        </div>
+    );
 }
 
 export default App;
--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,22 +1,22 @@
-import { createBrowserRouter, type RouteObject } from "react-router";
+import {createBrowserRouter, type RouteObject} from "react-router";
 import Overview from "../pages/Overview";
-import { instanceRoutes } from "./instanceRoutes";
-import { workflowRoutes } from "./workflowRoutes";
+import {instanceRoutes} from "./instanceRoutes";
+import {workflowRoutes} from "./workflowRoutes";
 import App from "../App";
 
 const routes: RouteObject[] = [
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: <Overview />,
-      },
-      ...instanceRoutes,
-      ...workflowRoutes,
-    ],
-  },
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                index: true,
+                element: <Overview />,
+            },
+            ...instanceRoutes,
+            ...workflowRoutes,
+        ],
+    },
 ];
 
 export const router = createBrowserRouter(routes);
