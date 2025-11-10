de5c0c44b175e59bc9143d49ede07c62f191e5e4
Danielle Klaasen <d.klaasen@uva.nl>
2025-11-10 15:04:18 +0100

    Adjust configuration and add type declarations for the UI kit

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -2,11 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import tailwindcss from '@tailwindcss/vite'
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
     plugins: [
         react(),
         tailwindcss(),
+        dts({
+            insertTypesEntry: true,
+        }),
     ],
     build: {
         lib: {
