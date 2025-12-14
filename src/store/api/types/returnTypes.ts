01f3890faea78c9c7dc1a02dfb4315c107d8e85a
Gerrit Oomens <g.oomens@uva.nl>
2025-12-14 11:59:48 +0100

    DN-3532 feat: submit submissions, cache updates

--- src/store/api/types/returnTypes.ts
+++ src/store/api/types/returnTypes.ts
@@ -1,4 +1,5 @@
 import type {Answer, Submission} from "./submissions";
+import type {LocalString} from "~/hooks/useTranslate.ts";
 import type {WorkflowInstance} from "~/store/api/types/instances.ts";
 
 export type SaveAnswerResult = {
@@ -8,4 +9,15 @@ export type SaveAnswerResult = {
 
 export type ExecuteActionResult = {
     instance?: WorkflowInstance;
+};
+
+export type SubmitSubmissionResult = {
+    submission: Submission;
+    updatedInstance?: WorkflowInstance;
+    validationErrors: ValidationError[];
+};
+
+export type ValidationError = {
+    questionName: string;
+    validationMessage: LocalString;
 };
