de641b7a2e8f338e1ec4612605b4690c76f8b92a
Gerrit Oomens <g.oomens@uva.nl>
2026-01-08 07:51:31 +0100

    chore: improve main page

--- src/pages/Overview.tsx
+++ src/pages/Overview.tsx
@@ -1,20 +1,8 @@
-import {useState} from "react";
-
 import {Link} from "react-router";
 
-import {Checkbox, Text} from "@datanose/ui";
-
 function Overview() {
-    const [checked, setChecked] = useState(false);
     return (
         <div>
-            <h1>Overview</h1>
-            <nav>
-                <Link to="/workflows">Workflows</Link>
-                <Link to="/instance/1">Instance 1</Link>
-                <Link to="/instance/2">Instance 2</Link>
-                <Link to="/instance/3">Instance 3</Link>
-            </nav>
             <div
                 style={{
                     display: "flex",
@@ -23,10 +11,10 @@ function Overview() {
                 }}
             >
                 <p>Welcome to the Workflow UI Overview page.</p>
-                <p>Here are some examples to get started with the UI kit:</p>
-                <Checkbox isSelected={checked} onChange={setChecked} label="Example checkbox" />
-                <Text>The checkbox is {checked ? "checked" : "unchecked"}.</Text>
             </div>
+            <nav>
+                <Link to="/screens/project-overview">Project overview screen</Link>
+            </nav>
         </div>
     );
 }
