c75c5a5bcbe75a311573b90dbef3fead2678fbe7
Annes Negmel-Din <annesnour03@gmail.com>
2026-01-19 16:36:34 +0100

    Revert "DN-3519 chore: debug broken pipeline"
    This reverts commit 87ea522f21792e86cfde7f67ff5cdfe9322fcea2.

--- tests/dummy-api.ts
+++ tests/dummy-api.ts
@@ -6,16 +6,8 @@ import {projectsScreen} from "./data/projectsScreen.ts";
 const app = express();
 app.use(express.json());
 
-app.use(function (req, res, next) {
+app.use(function (_, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-    // Handle OPTIONS preflight requests
-    if (req.method === "OPTIONS") {
-        return res.sendStatus(200);
-    }
-
     next();
 });
 
