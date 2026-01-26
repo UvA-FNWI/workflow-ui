c0c8ecfc8df43875fd2a67e6caada3531a1598fa
Danielle Klaasen <d.klaasen@uva.nl>
2026-01-26 14:30:05 +0100

    DN-3424 Fix build

--- src/components/instance/PageControl.tsx
+++ src/components/instance/PageControl.tsx
@@ -24,7 +24,7 @@ export const PageControl = ({
     page,
     showTitle = true,
 }: PageControlProps) => {
-    const {l} = useTranslate("workflow");
+    const {l, t} = useTranslate("workflow");
     const {data: submission} = submissionsEndpoints.getSubmission.useQuery({
         instanceId,
         submissionId,
@@ -73,7 +73,7 @@ export const PageControl = ({
                                         <div className="mb-4">
                                             <div key={question.name}>
                                                 {l(question.text)}
-                                                {!question.isRequired && ` ${l("optional")}`}
+                                                {!question.isRequired && ` ${t("optional")}`}
                                             </div>
                                             <InputControl
                                                 value={field.value}
