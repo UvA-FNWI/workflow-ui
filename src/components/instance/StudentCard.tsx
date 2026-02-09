b630a053e8a708a9ea1daf22488eda827f22c0c6
Danielle Klaasen <d.klaasen@uva.nl>
2026-02-09 15:29:08 +0100

    DN-3581 Adjust Student Card styling

--- src/components/instance/StudentCard.tsx
+++ src/components/instance/StudentCard.tsx
@@ -1,4 +1,4 @@
-import {Card, Heading, Link, Skeleton, Text} from "@datanose/ui";
+import {Card, Heading, Icon, Link, Skeleton, Text} from "@datanose/ui";
 
 import {useTranslate} from "~/hooks/useTranslate.ts";
 
@@ -22,29 +22,36 @@ export function StudentCard({isLoading, studentName, studentEmail}: StudentCardP
     return (
         <Card>
             {isLoading && (
-                <div className="flex flex-col items-center">
-                    <Skeleton className="h-16 w-16 rounded-full" />
+                <>
+                    <div className="flex flex-col items-center">
+                        <Skeleton className="h-16 w-16 rounded-full" />
+                    </div>
                     <Skeleton className="mt-4 h-5 w-32" />
-                </div>
+                </>
             )}
             {!isLoading && (studentName || studentEmail) && (
-                <div className="flex flex-col items-center">
-                    {studentName && (
-                        <>
-                            <div className="mb-2 inline-flex h-16 w-16 items-center justify-center rounded-full bg-gray-200 align-middle font-medium text-gray-600">
-                                {getInitials(studentName)}
-                            </div>
-                            <Heading as="h3" size="sm">
-                                {studentName}
-                            </Heading>
-                        </>
-                    )}
+                <>
+                    <div className="mb-2 flex flex-col items-center">
+                        {studentName && (
+                            <>
+                                <div className="mb-2 inline-flex h-16 w-16 items-center justify-center rounded-full bg-gray-200 align-middle font-medium text-gray-600">
+                                    {getInitials(studentName)}
+                                </div>
+                                <Heading as="h3" size="sm">
+                                    {studentName}
+                                </Heading>
+                            </>
+                        )}
+                    </div>
                     {studentEmail && (
-                        <Link intent="destructive" href={`mailto:${studentEmail}`}>
-                            {studentEmail}
-                        </Link>
+                        <div className="flex items-center gap-2">
+                            <Icon name="email-line" />
+                            <Link href={`mailto:${studentEmail}`} underline>
+                                {studentEmail}
+                            </Link>
+                        </div>
                     )}
-                </div>
+                </>
             )}
             {!isLoading && !studentName && !studentEmail && (
                 <>
