54866c3a977c54db28a02464b2529893e7628117
Robin Bakker <rsa.bakker@uva.nl>
2026-02-21 19:44:08 +0100

    DN-3381 feat(core): remove optional Redux dependency for core library as we don't use it right now

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -14,22 +14,16 @@ export default defineConfig({
         lib: {
             entry: {
                 core: path.resolve(__dirname, "src/index.ts"),
-                redux: path.resolve(__dirname, "src/redux/index.ts"),
             },
             name: "DataNoseCore",
-            fileName: (format, entryName) =>
-                entryName === "redux"
-                    ? `redux/${entryName}.${format}.js`
-                    : `${entryName}.${format}.js`,
+            fileName: (format, entryName) => `${entryName}.${format}.js`,
         },
         rollupOptions: {
-            external: ["react", "react-dom", "@reduxjs/toolkit", "redux"],
+            external: ["react", "react-dom"],
             output: {
                 globals: {
                     react: "React",
                     "react-dom": "ReactDOM",
-                    redux: "Redux",
-                    "@reduxjs/toolkit": "RTK",
                 },
             },
         },
