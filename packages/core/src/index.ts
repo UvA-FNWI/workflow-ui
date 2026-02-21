7fb4470ac84e8509eb7d09d71b73939912870654
Robin Bakker <rsa.bakker@uva.nl>
2026-02-21 20:25:02 +0100

    DN-3381 fix(core): add some missing exports that are needed in DN2

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,5 @@
+// TODO: we shouldn't export authHelpers but for now we still need it in DN2
+export * as authHelpers from "./Auth.helpers";
 export {
     selectAuthUser,
     selectAuthStatus,
@@ -5,7 +7,8 @@ export {
     selectIsAuthenticated,
 } from "./Auth.selectors";
 export {authReducer} from "./Auth.reducer";
-// TODO: we may want to expose AuthState and some things in Auth.types? See what is needed and add exports here.
+// TODO: we may want to expose AuthState and also other types in Auth.types? See what is needed and add exports here.
+export type {CustomUserState} from "./Auth.types";
 export {AuthProvider} from "./AuthProvider";
-export * from "./AuthService";
+export * as AuthService from "./AuthService";
 export {useAuth} from "./useAuth";
