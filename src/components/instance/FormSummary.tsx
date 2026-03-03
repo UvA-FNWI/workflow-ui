d74f17e9f2bac7c940f57e304ec04b924185ac2e
Danielle Petersen <d.j.kwakkel@uva.nl>
2026-03-03 13:43:51 +0100

    DN-3625 fix: Only show tabs and summary for multiple pages

--- src/components/instance/FormSummary.tsx
+++ src/components/instance/FormSummary.tsx
@@ -1,10 +1,8 @@
 import {Button, Icon, Text} from "@datanose/ui";
-import type {FetchBaseQueryError} from "@reduxjs/toolkit/query";
 
 import {QuestionAnswerList} from "./QuestionAnswerList.tsx";
+import {FormSubmitButton} from "~/components/instance/FormSubmitButton.tsx";
 import {useTranslate} from "~/hooks/useTranslate.ts";
-import {submissionsEndpoints} from "~/store/api/submissionsApi.ts";
-import type {SubmitSubmissionResult} from "~/store/api/types/returnTypes.ts";
 import type {Submission} from "~/store/api/types/submissions.ts";
 
 type Props = {
@@ -16,8 +14,6 @@ type Props = {
 
 export const FormSummary = ({instanceId, submission, onEditPage, onSubmit}: Props) => {
     const {t, l} = useTranslate("workflow");
-
-    const [submitSubmission, {isLoading}] = submissionsEndpoints.submitSubmission.useMutation();
 
     return (
         <div className="flex flex-col gap-6">
@@ -62,33 +58,11 @@ export const FormSummary = ({instanceId, submission, onEditPage, onSubmit}: Prop
             ))}
 
             {onSubmit && (
-                <div className="mt-4 flex justify-between gap-2">
-                    <Button
-                        intent="primary"
-                        variant="destructive"
-                        className="ml-auto"
-                        isLoading={isLoading}
-                        onClick={async () => {
-                            const res = await submitSubmission({
-                                instanceId,
-                                submissionId: submission.id,
-                            });
-                            const errorResult = (res.error as FetchBaseQueryError)
-                                ?.data as SubmitSubmissionResult;
-                            if (errorResult?.validationErrors.length) {
-                                // TODO: validation (DN-3424)
-                                const question = errorResult.validationErrors[0];
-                                alert(
-                                    `Not valid! ${question.questionName}: ${l(question.validationMessage)}`,
-                                );
-                                return;
-                            }
-                            onSubmit();
-                        }}
-                    >
-                        {t("submit")}
-                    </Button>
-                </div>
+                <FormSubmitButton
+                    instanceId={instanceId}
+                    submission={submission}
+                    onSubmit={onSubmit}
+                />
             )}
         </div>
     );
