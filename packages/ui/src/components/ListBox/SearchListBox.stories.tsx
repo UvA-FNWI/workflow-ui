059d08ec9d8430ea23467555dfebced162163914
Danielle Petersen <d.j.kwakkel@uva.nl>
2026-03-16 13:32:04 +0100

    DN-3604 fix: Primary icon color to uva black

--- packages/ui/src/components/ListBox/SearchListBox.stories.tsx
+++ packages/ui/src/components/ListBox/SearchListBox.stories.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 
+import { Selection } from 'react-stately';
+
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { SearchListBox, SearchListBoxValue } from './SearchListBox';
