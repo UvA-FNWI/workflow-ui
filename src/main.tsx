4ae4ed05d6ceb9f3092e98e411a91abe46abb9d1
annes <76728727+annesnour03@users.noreply.github.com>
2026-03-30 14:46:44 +0200

    DN-3644 feat(canvas): add canvas auth handling (#52)
    * DN-3644 feat(canvas): add canvas auth handling
    
    * DN-3644 chore: remove server port
    
    * DN-3644 fix: build

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,8 @@ import {Provider} from "react-redux";
 import {RouterProvider} from "react-router";
 
 import {ThemeProvider, ToastProvider} from "@datanose/ui";
-import {AuthProvider} from "@uva-fnwi/datanose-core";
-import type {AuthEventCallbacks} from "@uva-fnwi/datanose-core";
+import "@datanose/ui/styles";
+import {type AuthEventCallbacks, AuthProvider} from "@uva-fnwi/datanose-core";
 
 import {
     VITE_AUTH_AUTHORITY,
