c3ef6617852925d3bde8ed28602e4f56143c7a8c
Danielle Petersen <d.j.kwakkel@uva.nl>
2026-04-13 15:04:11 +0200

    DN-3630 fix: Make Assessment Summary mobile responsive

--- src/components/instance/FormSummaryAssessment.tsx
+++ src/components/instance/FormSummaryAssessment.tsx
@@ -39,102 +39,114 @@ export const FormSummaryAssessment = ({
     const colsClass = colsList[assessmentForms?.length ?? 1];
 
     return (
-        <div className="flex flex-col gap-6">
-            {/* Assessment columns header */}
-            {assessmentForms.length > 1 && (
-                <div className={`grid gap-4 ${colsClass}`}>
-                    <div></div>
-                    {assessmentForms.map((assessment) => (
-                        <div key={assessment.id}>
-                            <Text fontWeight="normal" size="lg" intent="error">
-                                {l(assessment.formTitle)?.toUpperCase()}
-                            </Text>
-                        </div>
-                    ))}
-                </div>
-            )}
-            {submission.form.pages.map((page, index) => {
-                const allQuestionAnswerPairs = assessmentForms.map((a) =>
-                    getVisibleQuestionAnswerPairs(
-                        page.questions,
-                        a.answers,
-                        assessmentForms.find((f) => f.id === a.id)?.results?.[page.name],
-                    ),
-                );
-
-                return (
-                    <div key={index} className="contents">
-                        {/* Page title with edit button */}
-                        <div className={`grid gap-4 ${colsClass}`}>
-                            <div className="flex items-center">
-                                <Text fontWeight="semibold" size="lg">
-                                    {l(page.title)?.toUpperCase()}
-                                    {assessmentForms[0]?.results?.[page.name]?.length &&
-                                        ` (${assessmentForms[0].results?.[page.name].reduce((sum, q) => sum + q.percentage, 0).toLocaleString(i18n.language)}%)`}
+        <div className="overflow-x-auto">
+            <div className="flex min-w-xl flex-col gap-6">
+                {/* Assessment columns header */}
+                {assessmentForms.length > 1 && (
+                    <div className={`grid gap-4 ${colsClass}`}>
+                        <div></div>
+                        {assessmentForms.map((assessment) => (
+                            <div key={assessment.id} className="w-48">
+                                <Text fontWeight="normal" size="lg" intent="error">
+                                    {l(assessment.formTitle)?.toUpperCase()}
                                 </Text>
-                                {onEditPage && formType === "Normal" && page.isInCurrentForm && (
-                                    <Button
-                                        intent="ghost"
-                                        size="small"
-                                        shape="circular"
-                                        className="ui:border-0 ui:hover:enabled:bg-grey-100 ui:dark:hover:enabled:bg-grey-800"
-                                        onClick={() => onEditPage(index)}
-                                        rightIcon={
-                                            <Icon name="edit-line" size="xs" color="danger" />
-                                        }
-                                        aria-label={t("instance.summary.edit_page", {
-                                            pageTitle: l(page.title),
-                                        })}
-                                    ></Button>
-                                )}
                             </div>
-
-                            {assessmentForms.map((assessment) => (
-                                <div key={assessment.id}>
-                                    {assessment?.weightedAverages[page.name] ? (
-                                        <Text fontWeight="bold" size="lg">
-                                            {assessment.weightedAverages[page.name].toLocaleString(
-                                                i18n.language,
-                                            )}
-                                        </Text>
-                                    ) : (
-                                        <Text fontWeight="bold" size="lg">
-                                            -
-                                        </Text>
-                                    )}
-                                </div>
-                            ))}
-                        </div>
-                        {/* Questions and answers */}
-                        {allQuestionAnswerPairs.flat().length > 0 && (
-                            <QuestionAnswerList
-                                questionAnswerPairs={allQuestionAnswerPairs}
-                                noAnswerText={t("instance.summary.no_answer")}
-                                instanceId={instanceId}
-                                submissionId={submission.id}
-                                colsClass={colsClass}
-                                collapseAnswers={true}
-                            />
-                        )}
-                        <Separator
-                            weight={index == submission.form.pages.length - 1 ? "bold" : "normal"}
-                        />
+                        ))}
                     </div>
-                );
-            })}
+                )}
+                {submission.form.pages.map((page, index) => {
+                    const allQuestionAnswerPairs = assessmentForms.map((a) =>
+                        getVisibleQuestionAnswerPairs(
+                            page.questions,
+                            a.answers,
+                            assessmentForms.find((f) => f.id === a.id)?.results?.[page.name],
+                        ),
+                    );
 
-            {assessmentForms.length > 0 && (
-                <div className={`grid gap-4 ${colsClass}`}>
-                    <Text fontWeight="bold" size="lg">
-                        {t("instance.calculations.final_grade").toUpperCase()}
-                    </Text>
-                    {assessmentForms.map((assessment) => (
-                        <Text fontWeight="bold" size="lg" key={assessment.id}>
-                            {assessment?.weightedAverages["total"].toLocaleString(i18n.language)}
+                    return (
+                        <div key={index} className="contents">
+                            {/* Page title with edit button */}
+                            <div className={`grid gap-4 ${colsClass} w-full max-w-full`}>
+                                <div className="flex items-center">
+                                    <Text fontWeight="semibold" size="lg">
+                                        {l(page.title)?.toUpperCase()}
+                                        {assessmentForms[0]?.results?.[page.name]?.length &&
+                                            ` (${assessmentForms[0].results?.[page.name].reduce((sum, q) => sum + q.percentage, 0).toLocaleString(i18n.language)}%)`}
+                                    </Text>
+                                    {onEditPage &&
+                                        formType === "Normal" &&
+                                        page.isInCurrentForm && (
+                                            <Button
+                                                intent="ghost"
+                                                size="small"
+                                                shape="circular"
+                                                className="ui:border-0 ui:hover:enabled:bg-grey-100 ui:dark:hover:enabled:bg-grey-800"
+                                                onClick={() => onEditPage(index)}
+                                                rightIcon={
+                                                    <Icon
+                                                        name="edit-line"
+                                                        size="xs"
+                                                        color="danger"
+                                                    />
+                                                }
+                                                aria-label={t("instance.summary.edit_page", {
+                                                    pageTitle: l(page.title),
+                                                })}
+                                            ></Button>
+                                        )}
+                                </div>
+
+                                {assessmentForms.map((assessment) => (
+                                    <div key={assessment.id}>
+                                        {assessment?.weightedAverages[page.name] ? (
+                                            <Text fontWeight="bold" size="lg">
+                                                {assessment.weightedAverages[
+                                                    page.name
+                                                ].toLocaleString(i18n.language)}
+                                            </Text>
+                                        ) : (
+                                            <Text fontWeight="bold" size="lg">
+                                                -
+                                            </Text>
+                                        )}
+                                    </div>
+                                ))}
+                            </div>
+                            {/* Questions and answers */}
+                            {allQuestionAnswerPairs.flat().length > 0 && (
+                                <QuestionAnswerList
+                                    questionAnswerPairs={allQuestionAnswerPairs}
+                                    noAnswerText={t("instance.summary.no_answer")}
+                                    instanceId={instanceId}
+                                    submissionId={submission.id}
+                                    colsClass={colsClass}
+                                    collapseAnswers={true}
+                                />
+                            )}
+                            <Separator
+                                weight={
+                                    index == submission.form.pages.length - 1 ? "bold" : "normal"
+                                }
+                            />
+                        </div>
+                    );
+                })}
+
+                {assessmentForms.length > 0 && (
+                    <div className={`grid gap-4 ${colsClass}`}>
+                        <Text fontWeight="bold" size="lg">
+                            {t("instance.calculations.final_grade").toUpperCase()}
                         </Text>
-                    ))}
-                </div>
-            )}
+                        {assessmentForms.map((assessment) => (
+                            <Text fontWeight="bold" size="lg" key={assessment.id}>
+                                {assessment?.weightedAverages["total"].toLocaleString(
+                                    i18n.language,
+                                )}
+                            </Text>
+                        ))}
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
