b404191741ba1fcf10e8e8e10bfaab5dfb4adc74
annes <76728727+annesnour03@users.noreply.github.com>
2026-04-22 12:02:57 +0200

    Feature/dn 3677 (#62)
    * DN-3677 feat: add header status to workflow steps and update StepCard component to display status pills
    
    * DN-3677 fix: update StepCard to conditionally display date completed based on header status

--- src/store/api/types/instances.ts
+++ src/store/api/types/instances.ts
@@ -25,6 +25,11 @@ export type WorkflowInstance = {
     viewerRoles: string[];
 };
 
+export type StepHeaderStatus = {
+    type: "Info" | "Attention" | "Success";
+    label: LocalString;
+};
+
 export type WorkflowStep = {
     id: string;
     title: LocalString;
@@ -34,6 +39,7 @@ export type WorkflowStep = {
     deadline: string | null;
     children: WorkflowStep[] | null;
     versions: WorkflowStepVersion[] | null;
+    headerStatus: StepHeaderStatus | null;
 };
 
 export type WorkflowStepVersion = {
