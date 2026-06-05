0bd84d6af53c54fef8ef27563ce102d573a0fabe
annes <76728727+annesnour03@users.noreply.github.com>
2026-06-05 11:58:02 +0200

    DN-3760 feature(users): verify external emails (#73)
    * DN-3760 feature(users): verify external emails
    
    * DN-3760 DN-3761 feat: enhance UserPicker and AddExternalUserModal to support external user creation
    
    * DN-3760 chore: reorder
    
    * DN-3760 chore: revert back to original description
    
    * DN-3760 feat(UserPicker): enhance user display with email and organization information
    
    * DN-3760 feat(UserPicker): add support for external users in user selection and search functionality
    
    ---------
    
    Co-authored-by: Gerrit Oomens <g.oomens@uva.nl>

--- src/store/api/types/users.ts
+++ src/store/api/types/users.ts
@@ -2,8 +2,9 @@ export interface UserSearchResult {
     userName: string;
     displayName: string;
     email: string;
-    organization?: Organization;
+    organization?: Organization | null;
     isExternal: boolean;
+    sourceKey?: string | null;
 }
 
 export interface CurrentUserResponse extends UserSearchResult {
@@ -13,4 +14,19 @@ export interface CurrentUserResponse extends UserSearchResult {
 export interface Organization {
     id: string;
     name: string;
+}
+
+export interface VerifyEmailRequest {
+    email: string;
+}
+
+export interface VerifyEmailResponse {
+    email: string;
+    status: string;
+}
+
+export interface CreateExternalUserInput {
+    displayName: string;
+    email: string;
+    organization?: Organization | null;
 }
