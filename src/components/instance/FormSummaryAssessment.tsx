d1f6ad4aadf3d62571b8bbc28da92c017d962095
Gerrit Oomens <g.oomens@uva.nl>
2026-06-23 08:26:46 +0200

    DN-3834 fix: don't show empty results

--- src/components/instance/FormSummaryAssessment.tsx
+++ src/components/instance/FormSummaryAssessment.tsx
@@ -48,38 +48,38 @@ export const FormSummaryAssessment = ({
         );
     }
 
-    if (step?.resultsType === "AssessmentFinalOverview")
-        return (
-            <div className="mt-4 flex flex-col gap-2">
-                {assessmentResults.parts.map((part) => (
-                    <div className="grid grid-cols-2 gap-4">
-                        <Text size="md" fontWeight="semibold" className="py-1">
-                            {`${l(part.title)} (${part.percentage}%):`}
-                        </Text>
-                        <Text size="md" className="py-1" fontWeight="semibold">
-                            {part.combined?.weightedAverage}
-                        </Text>
-                    </div>
-                ))}
+    if (step?.resultsType === "AssessmentFinalOverview") if (!assessmentResults.finalGrade) return;
+    return (
+        <div className="mt-4 flex flex-col gap-2">
+            {assessmentResults.parts.map((part) => (
                 <div className="grid grid-cols-2 gap-4">
-                    <Text size="lg" fontWeight="semibold" className="py-1">
-                        {t("instance.calculations.final_grade")}:
+                    <Text size="md" fontWeight="semibold" className="py-1">
+                        {`${l(part.title)} (${part.percentage}%):`}
                     </Text>
-                    <Text size="lg" fontWeight="semibold" className="py-1">
-                        {assessmentResults?.finalGrade ?? "-"}
+                    <Text size="md" className="py-1" fontWeight="semibold">
+                        {part.combined?.weightedAverage}
                     </Text>
                 </div>
-                <Separator weight="bold" className="my-4" />
-                {submissions[0]?.form.pages.length == 1 && (
-                    <PageControl
-                        instanceId={instanceId}
-                        submissionId={submissions[0].id}
-                        page={submissions[0].form.pages[0]}
-                        showTitle={false}
-                    />
-                )}
+            ))}
+            <div className="grid grid-cols-2 gap-4">
+                <Text size="lg" fontWeight="semibold" className="py-1">
+                    {t("instance.calculations.final_grade")}:
+                </Text>
+                <Text size="lg" fontWeight="semibold" className="py-1">
+                    {assessmentResults?.finalGrade ?? "-"}
+                </Text>
             </div>
-        );
+            <Separator weight="bold" className="my-4" />
+            {submissions[0]?.form.pages.length == 1 && (
+                <PageControl
+                    instanceId={instanceId}
+                    submissionId={submissions[0].id}
+                    page={submissions[0].form.pages[0]}
+                    showTitle={false}
+                />
+            )}
+        </div>
+    );
 
     const assessmentSubmissions = assessmentResults.parts
         .flatMap((part) => [...(part.sourceResults ?? []), part.combined])
