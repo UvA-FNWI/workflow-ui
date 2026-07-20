8505b4c79c0c533772a2433b9bbf173509999829
Danielle Petersen <d.j.kwakkel@uva.nl>
2026-07-20 11:01:47 +0200

    DN-3842 fix: Fix console logging about nested p elements

--- src/components/instance/PageControl.tsx
+++ src/components/instance/PageControl.tsx
@@ -151,7 +151,7 @@ export const PageControl = ({
                         </Heading>
                     )}
                     {page.introduction && (
-                        <Text size="lg">
+                        <Text size="lg" as="span">
                             <MarkdownRenderer>{l(page.introduction) ?? ""}</MarkdownRenderer>
                         </Text>
                     )}
