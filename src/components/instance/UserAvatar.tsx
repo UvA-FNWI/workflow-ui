36e4be5b6ecc19d820819d981fddba7af4bf9006
Danielle Petersen <d.j.kwakkel@uva.nl>
2026-07-30 13:32:34 +0200

    DN-3840 feature: Show Avatar Initials as placeholder during loading

--- src/components/instance/UserAvatar.tsx
+++ src/components/instance/UserAvatar.tsx
@@ -1,3 +1,5 @@
+import {useState} from "react";
+
 const getInitials = (name: string): string => {
     return name
         .trim()
@@ -6,18 +8,31 @@ const getInitials = (name: string): string => {
         .map((word) => word.charAt(0).toUpperCase() + ".")
         .join(" ");
 };
+
 interface UserAvatarProps {
     userName: string;
     picture?: string;
 }
+function AvatarInitials({userName}: {userName: string}) {
+    return <span className="w-full p-1 text-center">{getInitials(userName)}</span>;
+}
+
 export function UserAvatar({userName, picture}: UserAvatarProps) {
-    console.log(picture);
+    const [imageLoaded, setImageLoaded] = useState(false);
     return (
         <div className="mb-2 inline-flex h-16 w-16 items-center justify-center overflow-hidden rounded-full bg-gray-200 align-middle font-medium text-gray-600">
             {picture ? (
-                <img src={picture} alt={userName} className="h-full w-full object-cover" />
+                <>
+                    {!imageLoaded && <AvatarInitials userName={userName} />}
+                    <img
+                        src={picture}
+                        alt={userName}
+                        className={`h-full w-full object-cover ${!imageLoaded && "hidden"}`}
+                        onLoad={() => setImageLoaded(true)}
+                    />
+                </>
             ) : (
-                <span className="w-full p-1 text-center">{getInitials(userName)}</span>
+                <AvatarInitials userName={userName} />
             )}
         </div>
     );
