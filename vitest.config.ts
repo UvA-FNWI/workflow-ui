eb64d6e5e25250e19e1cb83c01f8db8b400e807a
Michel Roos <m.roos3@uva.nl>
2026-08-04 12:30:50 +0200

    DN-3904 Trying to fix NavbarUser test

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,9 +3,13 @@ import {defineConfig} from "vitest/config";
 
 export default defineConfig({
     resolve: {
-        alias: {
-            "~": path.resolve(__dirname, "src"),
-        },
+        alias: [
+            {
+                find: /^@uva-fnwi\/datanose-ui$/,
+                replacement: path.resolve(__dirname, "packages/ui/src/index.ts"),
+            },
+            {find: "~", replacement: path.resolve(__dirname, "src")},
+        ],
     },
     test: {
         environment: "jsdom",
