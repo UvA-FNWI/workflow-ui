1286e8ebcbd893322955ac578d1e6a7b9f24efd7
Danielle Petersen <d.j.kwakkel@uva.nl>
2026-08-06 10:28:42 +0200

    DN-3755 feat: UI of first two steps in import modal

--- src/components/Import/ImportColumnSelection.tsx
+++ src/components/Import/ImportColumnSelection.tsx
@@ -1,26 +1,118 @@
-import {Callout, Text} from "@uva-fnwi/datanose-ui";
+import {type Key, useState} from "react";
+
+import {Button, Callout, Checkbox, Icon, Item, Select, Text} from "@uva-fnwi/datanose-ui";
 
 import {useTranslate} from "~/hooks/useTranslate.ts";
 
 type ImportColumnSelectionProps = {
     columns: string[];
+    fileName: string;
+    onRemoveFile?: () => void;
 };
 
-export const ImportColumnSelection = ({columns}: ImportColumnSelectionProps) => {
+type ImportColumnSelectionRowProps = {
+    name: string;
+    columns: string[];
+    selectedColumn: string;
+    disabledKeys?: string[];
+    onSelect: (value: Key | null) => void;
+};
+
+export const ImportColumnSelection = ({
+    columns,
+    fileName,
+    onRemoveFile,
+}: ImportColumnSelectionProps) => {
     const {t} = useTranslate("screens", {keyPrefix: "import"});
+    const {t: tw} = useTranslate("workflow");
+    const [selectedColumns, setSelectedColumns] = useState<Record<string, string>>(() =>
+        Object.fromEntries(columns.map((col) => [col, ""])),
+    );
+
+    const handleColumnSelect = (col: string) => (key: Key | null) => {
+        setSelectedColumns((prev) => ({...prev, [col]: key !== null ? String(key) : ""}));
+    };
 
     return (
         <div>
-            <Text>file name</Text>
-            <Text>{t("select_student_number")}</Text>
-            {/*<SelectInput state={} valueProps={} triggerProps={} triggerRef={}/>*/}
-            <div>
-                <Text>{t("column_selection")}</Text>
+            <div className="flex items-center bg-grey-200 px-4 py-2 dark:bg-grey-800 dark:text-white">
+                <Text size="sm" decoration="underline">
+                    {fileName}
+                </Text>
+                <Button
+                    intent="ghost"
+                    onClick={onRemoveFile}
+                    aria-label="Remove file"
+                    size="square"
+                    className="ml-auto"
+                >
+                    <Icon name="trash-line" size="md" color="current" />
+                </Button>
+            </div>
+            <div className="flex flex-col gap-2 py-4">
+                <Text fontWeight="bold">{t("select_student_number")}</Text>
+                <Select
+                    value={selectedColumns["Studentnummer"]}
+                    onChange={handleColumnSelect("Studentnummer")}
+                    placeholder={tw("select")}
+                >
+                    {columns.map((col) => (
+                        <Item key={col}>{col}</Item>
+                    ))}
+                </Select>
+            </div>
+
+            <div className="flex flex-col gap-2 py-4">
+                <Text fontWeight="bold">{t("column_selection")}</Text>
                 {columns.map((col) => (
-                    <Text>{col}</Text>
+                    <ImportColumnSelectionRow
+                        name={col}
+                        key={col}
+                        columns={columns}
+                        selectedColumn={selectedColumns[col]}
+                        onSelect={handleColumnSelect(col)}
+                        disabledKeys={Object.entries(selectedColumns)
+                            .filter(([key, val]) => key !== col && val !== "")
+                            .map(([, val]) => val)}
+                    />
                 ))}
             </div>
             <Callout>{t("staff_info_callout")}</Callout>
+        </div>
+    );
+};
+
+const ImportColumnSelectionRow = ({
+    name,
+    selectedColumn,
+    onSelect,
+    columns,
+    disabledKeys = [],
+}: ImportColumnSelectionRowProps) => {
+    const {t: tw} = useTranslate("workflow");
+    const [isSelected, setIsSelected] = useState(false);
+    return (
+        <div className="flex h-10 w-full gap-4" key={name}>
+            <Checkbox
+                label={name}
+                isSelected={isSelected}
+                onChange={() => setIsSelected(!isSelected)}
+            />
+            {isSelected && (
+                <div className="ml-auto">
+                    <Select
+                        value={selectedColumn}
+                        onChange={onSelect}
+                        placeholder={tw("select")}
+                        disabledKeys={disabledKeys}
+                        className="h-10 w-40"
+                    >
+                        {columns.map((col) => (
+                            <Item key={col}>{col}</Item>
+                        ))}
+                    </Select>
+                </div>
+            )}
         </div>
     );
 };
--- src/components/Import/ImportModal.tsx
+++ src/components/Import/ImportModal.tsx
@@ -2,6 +2,8 @@ import {useState} from "react";
 
 import {Button, Icon, Modal, Text} from "@uva-fnwi/datanose-ui";
 
+import {ImportColumnSelection} from "~/components/Import/ImportColumnSelection.tsx";
+import {ImportOverview} from "~/components/Import/ImportOverview.tsx";
 import {useTranslate} from "~/hooks/useTranslate.ts";
 
 type ImportModalProps = {
@@ -12,8 +14,8 @@ type ImportModalProps = {
 export const ImportModal = ({isOpen, onClose}: ImportModalProps) => {
     const {t} = useTranslate("screens", {keyPrefix: "import"});
     const {t: tw} = useTranslate("workflow");
-    const [hasUploaded, setHasUploaded] = useState(false);
     const [activeStep, setActiveStep] = useState(1);
+    const [selectedFile, setSelectedFile] = useState<string | null>(null);
 
     const totalSteps = 3;
     const nextStep = () => {
@@ -24,36 +26,75 @@ export const ImportModal = ({isOpen, onClose}: ImportModalProps) => {
         }
     };
 
+    const handleRemoveFile = () => {
+        console.log("Remove file");
+        setSelectedFile(null);
+        setActiveStep(1);
+    };
+
+    const handleCancel = () => {
+        onClose();
+        setActiveStep(1);
+        setSelectedFile(null);
+    };
+
+    const columns = [
+        "Studentnummer",
+        "Vakcode",
+        "Verkort onderwerp",
+        "1e beoordelaar",
+        "2e beoordelaar",
+        "extra rol o.b.v. config",
+        "Examinator",
+    ];
+
     return (
         <Modal isOpen={isOpen} onOpenChange={onClose}>
             <Modal.Header subTitle={t("step_indicator", {index: activeStep, total: totalSteps})}>
                 {t("title")}
             </Modal.Header>
             <Modal.Body>
-                <div className="flex flex-col gap-4">
+                <div className="flex flex-col gap-4 pr-8">
                     <Text>{t("description")}</Text>
-                    <Button
-                        intent="secondary"
-                        leftIcon={<Icon name="download-solid" size="sm" color="current" />}
-                        className="w-fit"
-                        size="large"
-                        onClick={() => setHasUploaded(true)}
-                    >
-                        {t("upload_file")}
-                    </Button>
+                    {activeStep === 1 && (
+                        <Button
+                            intent="secondary"
+                            leftIcon={<Icon name="download-solid" size="sm" color="current" />}
+                            className="w-fit"
+                            size="large"
+                            onClick={() => {
+                                setSelectedFile("Bestandsnaam.pdf");
+                            }}
+                        >
+                            {t("upload_file")}
+                        </Button>
+                    )}
+                    {activeStep === 2 && (
+                        <ImportColumnSelection
+                            columns={columns}
+                            fileName={selectedFile ?? ""}
+                            onRemoveFile={handleRemoveFile}
+                        />
+                    )}
+                    {activeStep === 3 && <ImportOverview data={columns} />}
                 </div>
             </Modal.Body>
             <Modal.Footer>
                 <Button
                     intent="primary"
                     variant="destructive"
-                    disabled={!hasUploaded}
+                    disabled={!selectedFile}
                     onClick={nextStep}
                     size="large"
                 >
                     {tw("confirm")}
                 </Button>
-                <Button intent="secondary" variant="destructive" size="large" onClick={onClose}>
+                <Button
+                    intent="secondary"
+                    variant="destructive"
+                    size="large"
+                    onClick={handleCancel}
+                >
                     {tw("cancel")}
                 </Button>
             </Modal.Footer>
